c4343b6f08b7558cf3c51e50966a9847be27b05f
Tomkode <79474984+Tomkode@users.noreply.github.com>
2024-03-22 18:15:12 +0200

    Delete A2/A2 directory

--- A2/A2/src/Model/Statement/IStmt.java
+++
@@ -1,9 +0,0 @@
-package Model.Statement;
-
-import Model.MyException;
-import Model.PrgState;
-
-public interface IStmt {
-    PrgState execute(PrgState state) throws MyException;
-    IStmt deepcopy();
-}
--- A2/A2/src/Repository/IRepository.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import Model.MyException;
-import Model.PrgState;
-
-public interface IRepository {
-    PrgState getCrtPrg() throws MyException;
-    void add(PrgState prg);
-}
